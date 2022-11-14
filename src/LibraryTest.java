dd19c349158c044cf5365ab388f6c9adbe9b2fdc
Stephen <93441585+Spuddytime@users.noreply.github.com>
2022-11-14 01:34:27 +0000

    PROJECT-9 new commit

---
+++ src/LibraryTest.java
@@ -0,0 +1,29 @@
+//Name:Stephen Walsh ID:21334234 done in IntelliJ with Java 18
+//Integrated into Jira for ease of access: KEY for integration is PROJECT-9
+
+
+//This is for testing the methods etc.. they should fail first
+
+//Will need to come back to this, so I can figure out how JUnit works
+
+public class LibraryTest {
+    // Not exactly sure how this works tbh
+
+    static Book a;
+    static Book b;
+    static Book c;
+    static Book d;
+    static Departments scienceDepartnment;
+    static Departments historyDepartnment;
+    static Departments compsciDepartnment;
+    static Departments geographyDepartment;
+    static Departments artDepartnment;
+
+    @BeforeAll // I believe this means this will run all the time before any tests take place
+    @Test // Place this before all tests to indicate it is a test
+
+
+
+
+
+}
